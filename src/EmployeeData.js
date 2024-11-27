84fa0e6d28e0fe1d6b56ce5456191228a27f2b3a
Yashwant Thakare <yashwantthakare4@gmail.com>
2024-11-27 19:34:16 +0530

    file.js added

---
+++ src/EmployeeData.js
@@ -0,0 +1,27 @@
+export const EmployeeData =[
+
+    {
+       id:1,
+       firstName:'Mark',
+       lastName:'David',
+       age:20
+    },
+    {
+        id:2,
+        firstName:'amol',
+        lastName:'Dudane',
+        age:29
+     },
+     {
+        id:3,
+        firstName:'Prashant ',
+        lastName:'Fahave',
+        age:24
+     }
+     ,{
+        id:4,
+        firstName:'Yash',
+        lastName:'Patil',
+        age:25
+     }
+]
